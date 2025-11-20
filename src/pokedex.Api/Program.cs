47ca36b51dc85b863c1650739a9151a5cfb33099
e-zoob <enrico.zoboli@outlook.com>
2025-11-20 17:40:57 +0100

    chore: add swagger

--- src/pokedex.Api/Program.cs
+++ src/pokedex.Api/Program.cs
@@ -1,5 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "Pokedex Api",
+        Version = "v1",
+        Description = "Minimal Api to get Pokemon information"
+    });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
