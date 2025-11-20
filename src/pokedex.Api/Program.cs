b81563b489a6a2ca27ed3c3c96443fc0f6822cf7
e-zoob <enrico.zoboli@outlook.com>
2025-11-20 18:19:41 +0100

    chore: add Serilog with bootstrap logging

--- src/pokedex.Api/Program.cs
+++ src/pokedex.Api/Program.cs
@@ -1,4 +1,20 @@
+using Serilog;
+using Serilog.Events;
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, lc) => lc
+    .ReadFrom.Configuration(builder.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -11,6 +27,10 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+try
+{
+    Log.Information("Starting Pokedex Api");
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -22,3 +42,13 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+}
+catch( Exception ex)
+{
+    Log.Fatal(ex, "Pokedex Api failed to start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
