c1f2b5481c81373d6e5ea1d9c2a1a299791a318d
e-zoob <enrico.zoboli@outlook.com>
2025-11-21 11:56:11 +0100

    tests: add test for pokemon name validator

---
+++ tests/PokedexApi.Tests/PokemonNameValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using pokedex.Api.Validation;
+
+
+namespace PokedexApi.Tests;
+
+public class PokemonNameValidatorTests
+{
+    private readonly PokemonNameValidator validator;
+
+    public PokemonNameValidatorTests()
+    {
+        validator = new();
+    }
+
+    [Theory]
+    [InlineData("pikachu")]
+    [InlineData("DITTO")]
+    [InlineData("aegislash")]
+    [InlineData("muk")]
+    [InlineData("Crabominable")]
+    public void Valid_Name_Shoul_Pass(string name)
+    {
+        // Act
+        var result = validator.TestValidate(name);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("     ")]
+    public void Empty_Or_WhiteSpace_Name_Should_Fail(string name)
+    {
+        // Act
+        var result = validator.TestValidate(name);
+        
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(x => x)
+            .WithErrorMessage("Name cannot be empty.");
+    }
+
+
+    [Theory]
+    [InlineData("p@ikachu")]
+    [InlineData("dr feelgood")]
+    [InlineData("mewtwo!")]
+    public void Invalid_Characters_Should_Fail(string name)
+    {
+        // Act
+        var result = validator.TestValidate(name);
+        
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(x => x)
+            .WithErrorMessage("Name must contain only letters.");
+    }
+
+    [Theory]
+    [InlineData("me")]
+    [InlineData("mypokemonnameistoolong")]
+    public void Invalid_Length_Should_Fail(string name)
+    {
+        // Act
+        var result = validator.TestValidate(name);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.ShouldHaveValidationErrorFor(x => x)
+            .WithErrorMessage("Name length must be between 3 and 20 characters.");
+    }
+}
