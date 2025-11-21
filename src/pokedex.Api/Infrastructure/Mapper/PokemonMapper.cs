f49638db8f5c3abaf4c31a0bc3bbd7dbdb2bc8e7
e-zoob <enrico.zoboli@outlook.com>
2025-11-21 13:10:23 +0100

    feat: add pokemon mapper

---
+++ src/pokedex.Api/Infrastructure/Mapper/PokemonMapper.cs
@@ -0,0 +1,25 @@
+using System.Text.RegularExpressions;
+using pokedex.Api.Domain.Models;
+using pokedex.Api.Infrastructure.Models;
+
+namespace Pokedex.Api.Infrastructure.Mapper;
+
+public partial class PokemonMapper
+{
+    public PokemonInfoDto ToInfoDto(PokemonInfoApiModel model)
+    {
+        var englishEntry = model.FlavorTextEntries?
+            .FirstOrDefault( e => e.Language.Name == "en");
+
+        return new PokemonInfoDto
+        {
+            Name = model.Name,
+            Description = englishEntry != null ? CatchWhitespaceRegex().Replace(englishEntry.FlavorText, " ").Trim() : "No english description available",
+            Habitat = !string.IsNullOrWhiteSpace(model.Habitat?.Name) ? model.Habitat.Name : "Pokemon habitat is unknown",
+            IsLegendary = model.IsLegendary
+        };
+    }
+
+    [GeneratedRegex(@"\s+")]
+    private static partial Regex CatchWhitespaceRegex();
+}
