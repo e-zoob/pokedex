49e825ec28f1e089399d5ad5cc9b54d43a3f34c8
e-zoob <enrico.zoboli@outlook.com>
2025-11-21 12:11:43 +0100

    refactor: simplify validator

--- src/pokedex.Api/Validation/PokemonNameValidator.cs
+++ src/pokedex.Api/Validation/PokemonNameValidator.cs
@@ -7,15 +7,8 @@ public class PokemonNameValidator: AbstractValidator<string>
     public PokemonNameValidator()
     {
         RuleFor(x => x)
-            .NotEmpty()
-            .WithMessage("Name cannot be empty.");
-        
-        RuleFor(x => x)
-            .Matches("^[a-zA-Z]+$")
-            .WithMessage("Name must contain only letters.");
-        
-        RuleFor(x => x)
-            .Length(3,20)
-            .WithMessage("Name length must be between 3 and 20 characters.");
+            .NotEmpty().WithMessage("Name cannot be empty.")
+            .Matches("^[a-zA-Z]+$").WithMessage("Name must contain only letters.")
+            .Length(3,20).WithMessage("Name length must be between 3 and 20 characters.");
     }   
 }
