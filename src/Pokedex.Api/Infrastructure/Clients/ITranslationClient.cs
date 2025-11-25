91cf9428dee0f2593c82f65acf6af6498e51a40c
e-zoob <enrico.zoboli@outlook.com>
2025-11-25 15:58:31 +0800

    feat: add client for calling translation api

---
+++ src/Pokedex.Api/Infrastructure/Clients/ITranslationClient.cs
@@ -0,0 +1,6 @@
+namespace Pokedex.Api.Infrastructure.Clients;
+
+public interface ITranslationClient
+{
+    Task<HttpResponseMessage?> TranslateAsync(string text, string style); 
+}
---
+++ src/Pokedex.Api/Infrastructure/Clients/TranslationClient.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using Pokedex.Api.Options;
+
+namespace Pokedex.Api.Infrastructure.Clients;
+
+public class TranslationClient(
+    HttpClient httpClient,
+    IOptions<TranslationApiOptions> options
+) : ITranslationClient
+{
+    private readonly JsonSerializerOptions JsonOptions = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
+    public async Task<HttpResponseMessage?> TranslateAsync(string text, string style)
+    {
+        httpClient.BaseAddress ??= new Uri(options.Value.BaseUri);
+
+        using var content = new FormUrlEncodedContent(
+        [
+            new KeyValuePair<string, string>("text", text)
+        ]);
+
+        return await httpClient.PostAsync($"{style}.json", content);
+    }
+}
